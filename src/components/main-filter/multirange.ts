c9d0a1a99ea099cef09df283677e92405b24474e
Nazarii Vladyka <vladyka.nazarii@gmail.com>
2023-01-02 21:12:17 +0200

    OS-9 feat(filters): add multirange input logic

--- src/components/main-filter/multirange.ts
+++ src/components/main-filter/multirange.ts
@@ -25,19 +25,24 @@ export function setMultirange(component: HTMLElement): void {
 
   function connectButtonToInput(element: HTMLButtonElement): HTMLInputElement {
     if (element.classList.contains('left') && element.classList.contains('price')) {
+      fromPrice.max = toPrice.value;
       return fromPrice;
     }
     if (element.classList.contains('right') && element.classList.contains('price')) {
+      toPrice.min = fromPrice.value;
       return toPrice;
     }
     if (element.classList.contains('left') && element.classList.contains('stock')) {
+      fromStock.max = toStock.value;
       return fromStock;
     }
+    toStock.min = fromStock.value;
     return toStock;
   }
 
   const range = component.querySelector(".multi-range__range") as HTMLElement;
   const rangeButtons = [...component.querySelectorAll(".multi-range__btn") as NodeListOf<HTMLElement>];
+  const rangeInputs = [fromPrice, toPrice, fromStock, toStock];
 
   function findOppositeButton(currentButton: HTMLButtonElement): HTMLButtonElement {
     if (currentButton.classList.contains('price')) {
@@ -57,18 +62,17 @@ export function setMultirange(component: HTMLElement): void {
     let maxPosition = 100;
     if (target.classList.contains('left')) {
       maxPosition = (range.clientWidth - target.clientWidth * 2) / range.clientWidth * 100 - 100 +
-      parseFloat(findOppositeButton(target).style.left.slice(0, -1));
-    }
-    if (target.classList.contains('right')) {
+      +(findOppositeButton(target).style.left.slice(0, -1));
+    } else {
       minPosition = target.clientWidth * 2 / range.clientWidth * 100 +
-      parseFloat(findOppositeButton(target).style.left.slice(0, -1));
+      +(findOppositeButton(target).style.left.slice(0, -1));
     }
 
-    const buttonPosition = parseFloat(target.style.left.slice(0, -1));
+    const buttonPosition = +(target.style.left.slice(0, -1));
     
     function onMouseMove(eventMove: MouseEvent): void {
       const offset = (eventMove.clientX - mousePosition) / range.clientWidth * 100;
-
+      
       if (buttonPosition + offset < minPosition) {
         target.style.left = `${ minPosition }%`;
         connectButtonToInput(target).value = connectButtonToInput(target).min;
@@ -78,7 +82,7 @@ export function setMultirange(component: HTMLElement): void {
       } else {
         target.style.left = `${ buttonPosition + offset }%`;
         connectButtonToInput(target).value =
-        `${ Math.round((parseInt(connectButtonToInput(target).max) - parseInt(connectButtonToInput(target).min)) *
+        `${ Math.round((+(connectButtonToInput(target).max) - +(connectButtonToInput(target).min)) *
         (offset + buttonPosition) / 100) }`;
       }
     }
@@ -92,4 +96,53 @@ export function setMultirange(component: HTMLElement): void {
     document.addEventListener('mouseup', onMouseUp);
   }
   rangeButtons.forEach(button => button.addEventListener('mousedown', moveButton));
+
+  function connectInputToButton(element: HTMLInputElement): HTMLButtonElement{
+    if (element.classList.contains('slider__from-data__input-price')) {
+      fromPrice.max = toPrice.value;
+      return rangeButtons.find(buttonToFind => buttonToFind.classList.contains('price') &&
+      buttonToFind.classList.contains('left')) as HTMLButtonElement;
+    }
+    if (element.classList.contains('slider__to-data__input-price')) {
+      toPrice.min = fromPrice.value;
+      return rangeButtons.find(buttonToFind => buttonToFind.classList.contains('price') &&
+      buttonToFind.classList.contains('right')) as HTMLButtonElement;
+    }
+    if (element.classList.contains('slider__from-data__input-stock')) {
+      fromStock.max = toStock.value;
+      return rangeButtons.find(buttonToFind => buttonToFind.classList.contains('stock') &&
+      buttonToFind.classList.contains('left')) as HTMLButtonElement;
+    }
+    toStock.min = fromStock.value;
+    return rangeButtons.find(buttonToFind => buttonToFind.classList.contains('stock') &&
+    buttonToFind.classList.contains('right')) as HTMLButtonElement;
+  }
+
+  function changeInput(eventChange: Event): void {
+    const targetInput = eventChange.target as HTMLInputElement;
+    const buttonInput = connectInputToButton(targetInput);
+    const oppositeInput = connectButtonToInput(findOppositeButton(buttonInput));
+    const offset = (buttonInput.clientWidth * 2) / range.clientWidth;
+
+    if (buttonInput.classList.contains('left')) {
+      if (+targetInput.value > +oppositeInput.value) {
+        targetInput.value = oppositeInput.value;
+      }
+      if (+targetInput.value < +targetInput.min) {
+        targetInput.value = targetInput.min;
+      }
+      buttonInput.style.left = `${(+targetInput.value - +targetInput.min) /
+      (+(oppositeInput.max) - +(targetInput.min)) * 100 * (1 - offset)}%`;
+    } else {
+      if (+targetInput.value < +oppositeInput.min) {
+        targetInput.value = oppositeInput.value;
+      }
+      if (+targetInput.value > +targetInput.max) {
+        targetInput.value = oppositeInput.min;
+      }
+      buttonInput.style.left = `${+targetInput.value / +targetInput.max *
+      100 * (1 - offset) + (offset * 100)}%`;
+    }
+  }
+  rangeInputs.forEach(input => input.addEventListener('input', changeInput));
 }
