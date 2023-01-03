dcf915f2e2b803f8f82d7b539c4e992166a55bbe
Maksym <94698037+MaksymKuzmych@users.noreply.github.com>
2023-01-03 13:20:22 +0200

    OS-13 feat(order-form): add order form with validation (#35)

---
+++ src/components/order-submit/check-payment.ts
@@ -0,0 +1,17 @@
+export function checkPayment(orderBackgroundDiv: HTMLElement) {
+  const cardNumberField = orderBackgroundDiv.querySelector('#card-number') as HTMLInputElement;
+  const paymentSystemImg = orderBackgroundDiv.querySelector('.card-details__img') as HTMLImageElement;
+
+  cardNumberField.addEventListener('input', () => {
+    switch (cardNumberField.value[0]) {
+      case '4':
+        paymentSystemImg.src = './assets/payment-systems/visa.svg';
+        break;
+      case '5':
+        paymentSystemImg.src = './assets/payment-systems/mastercard.svg';
+        break;
+      default:
+        paymentSystemImg.src = './assets/payment-systems/paypal.svg';
+    }
+  });
+}
